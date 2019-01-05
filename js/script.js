5e903da2a71a2e58ec2401d756edb017165e1ccf
lingjie zhang <zhanglingjie16303@gmail.com>
2019-01-04 20:24:31 -0500

    test methods

--- js/script.js
+++ js/script.js
@@ -155,6 +155,64 @@ function game() {
 
   }
 
+  function getBlankPosition() {
+    for (var i = 0; i < this.dimension; i++) {
+      for (var j = 0; j < this.dimension; j++) {
+        if (this.board[i][j] === 0) {
+          return [i, j];
+        }
+      }
+    }
+  }
+
+  function swap(i1, j1, i2, j2) {
+    let temp = this.board[i1][j1];
+    this.board[i1][j1] = this.board[i2][j2];
+    this.board[i2][j2] = temp;
+  }
+
+  function getMove(grid) {
+    let blankGrid = this.getBlankPosition();
+    let row = blankGrid[0];
+    let column = blankGrid[1];
+    if (row > 0 && this.board[row - 1][column] == grid) {
+      return "down";
+    } else if (row < this.dimension - 1 && this.board[row + 1][column] == grid) {
+      return "up";
+    } else if (column > 0 && this.board[row][column - 1] == grid) {
+      return "right";
+    } else if (column < this.dimension - 1 && this.board[row][column + 1] == grid) {
+      return "left";
+    }
+  }
+
+  // function move(grid) {
+  //   let move = this.getMove(grid);
+  //   if (move != null) {
+  //     var blankGrid = this.getBlankPosition();
+  //     var row = blankGrid[0];
+  //     var column = blankGrid[1];
+  //     switch (move) {
+  //       case "down":
+  //         this.swap(row, column, row - 1, column);
+  //         break;
+  //       case "up":
+  //         this.swap(row, column, row + 1, column);
+  //         break;
+  //       case "right":
+  //         this.swap(row, column, row, column - 1);
+  //         break;
+  //       case "left":
+  //         this.swap(row, column, row, column + 1);
+  //         break;
+  //     }
+  //     if (move != null) {
+  //         this.lastMove = grid;
+  //     }
+  //     return move;
+  //   }
+  // }
+
   function checkSolution() {
     if (tileMap.empty.position !== 9) return false;
 
